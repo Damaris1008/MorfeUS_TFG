e7c6b6abe77339f1820c76827dffad11a165baa2
Damaris1008 <damarisgomezse@gmail.com>
2023-01-19 20:36:20 +0100

    feat(develop): Audio settings added as an option in the main menu
    The player can now mute the background music and adjust the audio volume level.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
 
-    private Animator anim;
+    [Header("Audio Settings")]
+    [SerializeField] private Text volumeLevelText = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private GameObject muteMusicButton = null;
+    [SerializeField] private GameObject unmuteMusicButton = null;
     
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
-
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
@@ -23,5 +23,37 @@ public class MainMenuController : MonoBehaviour
         Application.Quit();
     }
 
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeLevelText.text = volume.ToString("0") + "%" ;
+    }
      
+    public void AudioSettingsApply()
+    {
+        PlayerPrefs.SetFloat("volumeLevel", AudioListener.volume);
+        
+        if(muteMusicButton.activeSelf){
+            PlayerPrefs.SetInt("backgroundMusic", 1); //Play music
+        }else{
+            PlayerPrefs.SetInt("backgroundMusic", 0); //Mute music
+        }
+    }
+
+    public void GetAudioPrefs()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volumeLevel");
+        volumeLevelText.text = PlayerPrefs.GetFloat("volumeLevel").ToString("0") + "%" ;
+
+        if(PlayerPrefs.GetInt("backgroundMusic") == 0)
+        {
+            unmuteMusicButton.SetActive(true);
+            muteMusicButton.SetActive(false);
+        }else{
+            unmuteMusicButton.SetActive(false);
+            muteMusicButton.SetActive(true);
+        }
+
+    }
+
 }
