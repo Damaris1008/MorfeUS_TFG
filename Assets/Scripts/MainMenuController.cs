6f2e9cf8f07572ca5136b9de52ddd507859d25f4
Damaris1008 <damarisgomezse@gmail.com>
2023-01-23 18:24:44 +0100

    feat(develop): Graphics settings menu functionality added
    Now the graphics settings menu saves and loads the preferences of the player.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -12,15 +12,46 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private Slider volumeSlider = null;
     [SerializeField] private GameObject muteMusicButton = null;
     [SerializeField] private GameObject unmuteMusicButton = null;
+    private bool _backgroundMusic;
 
     [Header("Graphics Settings")]
+    [SerializeField] private Dropdown qualityDropdown = null;
     [SerializeField] private Text brightnessLevelText = null;
     [SerializeField] private Slider brightnessSlider = null;
+    [SerializeField] private GameObject enableFullScreenButton = null;
+    [SerializeField] private GameObject disableFullScreenButton = null;
     private float _brightnessLevel;
     private bool _isFullScreen;
     private int _qualityLevel;
+
+    [Header("Resolution Dropdowns")]
+    public Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    public void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for(int i=0; i < resolutions.Length ; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     
-    public void PlayGame()
+    public void PlayNewGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
@@ -36,10 +67,17 @@ public class MainMenuController : MonoBehaviour
         volumeLevelText.text = volume.ToString("0") + "%" ;
     }
 
-    /*public void SetBrightness(float brightness)
+    public void SetBrightness(float brightness)
     {
         _brightnessLevel = brightness;
-        brightnessLevelText = brightness.ToString("0.0");
+        brightnessLevelText.text = brightness.ToString("0.0");
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        PlayerPrefs.SetInt("resolutionIndex", resolutionIndex);
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetFullscreen(bool isFullScreen)
@@ -47,20 +85,29 @@ public class MainMenuController : MonoBehaviour
         _isFullScreen = isFullScreen;
     }
 
+    public void SetBackgroundMusic(bool backgroundMusic)
+    {
+        _backgroundMusic = backgroundMusic;
+    }
+
     public void SetQuality(int qualityIndex)
     {
+        _qualityLevel = qualityIndex;
+    }
 
-    }*/
+    public void GraphicsApply()
+    {
+        PlayerPrefs.SetFloat("brightnessLevel", _brightnessLevel);
+        PlayerPrefs.SetInt("qualityLevel", _qualityLevel);
+        QualitySettings.SetQualityLevel(_qualityLevel);
+        PlayerPrefs.SetInt("fullscreen", (_isFullScreen ? 1:0));
+        Screen.fullScreen = _isFullScreen;
+    }
      
     public void AudioSettingsApply()
     {
         PlayerPrefs.SetFloat("volumeLevel", AudioListener.volume);
-        
-        if(muteMusicButton.activeSelf){
-            PlayerPrefs.SetInt("backgroundMusic", 1); //Play music
-        }else{
-            PlayerPrefs.SetInt("backgroundMusic", 0); //Mute music
-        }
+        PlayerPrefs.SetInt("backgroundMusic", (_backgroundMusic ? 1:0));
     }
 
     public void GetAudioPrefs()
@@ -76,7 +123,23 @@ public class MainMenuController : MonoBehaviour
             unmuteMusicButton.SetActive(false);
             muteMusicButton.SetActive(true);
         }
+    }
 
+    public void GetGraphicsPrefs()
+    {
+        qualityDropdown.value = PlayerPrefs.GetInt("qualityLevel");
+        resolutionDropdown.value = PlayerPrefs.GetInt("resolutionIndex");
+        brightnessSlider.value = PlayerPrefs.GetFloat("brightnessLevel");
+        brightnessLevelText.text = PlayerPrefs.GetFloat("brightnessLevel").ToString("0.0");
+
+        if(PlayerPrefs.GetInt("fullscreen") == 0)
+        {
+            disableFullScreenButton.SetActive(false);
+            enableFullScreenButton.SetActive(true);
+        }else{
+            disableFullScreenButton.SetActive(true);
+            enableFullScreenButton.SetActive(false);
+        }
     }
 
 }
