e08adaeafd98d79be5685f9c36489bf6929720d5
Damaris1008 <damarisgomezse@gmail.com>
2023-02-08 18:33:08 +0100

    feat(develop): Cursor customised

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -6,6 +6,10 @@ using UnityEngine.EventSystems;
 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    [Header("Cursor")]
+    public Texture2D cursorArrow;
+    public Texture2D selectCursor;
+
     [Header("UI")]
     public Image image;
     public Text countText;
@@ -39,6 +43,7 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 
     // Drag and drop
     public void OnBeginDrag(PointerEventData eventData){
+        Cursor.SetCursor(selectCursor, Vector2.zero, CursorMode.ForceSoftware);
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
@@ -46,10 +51,12 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     }
 
     public void OnDrag(PointerEventData eventData){
+        Cursor.SetCursor(selectCursor, Vector2.zero, CursorMode.ForceSoftware);
         transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData){
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
         transform.SetParent(parentAfterDrag);
         image.raycastTarget = true;
     }
