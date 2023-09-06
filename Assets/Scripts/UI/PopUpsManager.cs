e830f91105bdfe0e42ba34184933c88b9f0a4c90
Damaris1008 <damarisgomezse@gmail.com>
2023-09-06 18:19:37 +0200

    fix(develop): Little fix on game goal pop-up appearing time

--- Assets/Scripts/UI/PopUpsManager.cs
+++ Assets/Scripts/UI/PopUpsManager.cs
@@ -254,7 +254,7 @@ public class PopUpsManager : MonoBehaviour
     // GAME GOAL
 
     public IEnumerator ShowGameGoal(){
-        yield return new WaitForSeconds(4.0f);
+        yield return new WaitForSeconds(3.0f);
         gameGoal.SetActive(true);
         GameManager.PauseGame();
     }
